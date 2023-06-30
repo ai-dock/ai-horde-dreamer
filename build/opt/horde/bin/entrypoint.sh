897dfb709b4c037b2405b53b82b3ba802baa2a0a
Rob Ballantyne <rob@dynamedia.uk>
2023-06-30 07:45:50 +0100

    fix guard

--- build/opt/horde/bin/entrypoint.sh
+++ build/opt/horde/bin/entrypoint.sh
@@ -4,17 +4,13 @@ WORKSPACE="/workspace"
 DEFAULT_WORKER_NAME="Docker Dreamer"
 
 # Replace ___ with a space. Vast.ai fix
-while IFS='=' read -r -d '' n v; do
-    export ${n}="${v//___/' '}"
+while IFS='=' read -r -d '' key val; do
+    export ${key}="${val//___/' '}"
 done < <(env -0)
-
-printf "export PATH=\"$PATH\"\n" >> /root/.bashrc
 
 if [[ -z $GPU_COUNT ]]; then
     export GPU_COUNT=$(ls -l /proc/driver/nvidia/gpus/ | grep -c ^d)
 fi
-
-printf "export GPU_COUNT=$GPU_COUNT\n" >> /root/.bashrc
 
 if [[ -f "/root/.ssh/authorized_keys_mount" ]]; then
     cat /root/.ssh/authorized_keys_mount > /root/.ssh/authorized_keys
@@ -27,16 +23,12 @@ fi
 
 # Alt names for $SSH_PUBKEY
 # vast.ai
-if [[ -z $SSH_PUBLIC_KEY ]]; then
-    printf "$SSH_PUBKEY\n" >> /root/.ssh/authorized_keys
+if [[ ! -z $SSH_PUBLIC_KEY ]]; then
+    printf "$SSH_PUBLIC_KEY\n" >> /root/.ssh/authorized_keys
 fi
 
 # Don't run tmux automatically on vast.ai
 touch /root/.no_auto_tmux
-
-if [[ -z $HORDE_WORKER_BRANCH ]]; then
-    printf "HORDE_WORKER_BRANCH=\"$HORDE_WORKER_BRANCH\"\n" >> /root/bashrc
-fi
 
 # Ensure worker_name for bridge_stable_diffusion.py -n
 if [[ -z $BRIDGE_WORKER_NAME || "$BRIDGE_WORKER_NAME" = $DEFAULT_WORKER_NAME ]]; then
@@ -46,7 +38,6 @@ fi
 if [[ ! -z $BRIDGE_DREAMER_NAME ]]; then
     export BRIDGE_WORKER_NAME="${BRIDGE_DREAMER_NAME}"
 fi
-printf "export BRIDGE_WORKER_NAME=\"$BRIDGE_WORKER_NAME\"\n" >> /root/.bashrc
 
 # Terminal UI will not work in the default environment
 export BRIDGE_DISABLE_TERMINAL_UI=true
@@ -62,13 +53,18 @@ if [[ -d "$WORKSPACE" ]]; then
     ln -s "${WORKSPACE}/AI-Horde-Worker" /opt/AI-Horde-Worker
 fi
 
-# Ensure logged in users are in the dreamer mamba environment
-printf "micromamba activate dreamer\n" >> /root/.bashrc
-
 # Update the worker
 /opt/horde/bin/update-hordelib.sh
 /opt/horde/bin/update-horde-worker.sh
 
 micromamba run -n dreamer python /opt/horde/scripts/write-config.py
+
+# Ensure logged in users are in the dreamer mamba environment
+printf "micromamba activate dreamer\n" >> /root/.bashrc
+
+# Ensure all variables available for interactive sessions
+while IFS='=' read -r -d '' key val; do
+    printf "export %s=\"%s\"\n" "$key" "$val" >> /root/.bashrc
+done < <(env -0)
 
 exec "$@"
