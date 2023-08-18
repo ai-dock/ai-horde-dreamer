0fad278831e82d8d585e726205702425eff0c9bc
Rob Ballantyne <rob@dynamedia.uk>
2023-08-18 12:45:03 +0100

    Add jupyter kernel logic

--- build/COPY_ROOT/opt/ai-dock/bin/build/layer0/common.sh
+++ build/COPY_ROOT/opt/ai-dock/bin/build/layer0/common.sh
@@ -7,6 +7,7 @@ worker_git="https://github.com/Haidra-Org/AI-Horde-Worker"
 
 main() {
     create_env
+    install_jupyter_kernels
     clone_horde_worker
 }
 
@@ -18,6 +19,30 @@ create_env() {
     exported_env=/tmp/${MAMBA_DEFAULT_ENV}.yaml
     micromamba env export -n ${MAMBA_DEFAULT_ENV} > "${exported_env}"
     $MAMBA_CREATE -n horde --file "${exported_env}"
+}
+
+install_jupyter_kernels() {
+    if [[ $IMAGE_BASE =~ "jupyter-pytorch" ]]; then
+        $MAMBA_INSTALL -n horde -c conda-forge -c defaults -y \
+            ipykernel \
+            ipywidgets
+        
+        kernel_path=/usr/local/share/jupyter/kernels
+        
+        # Add the often-present "Python3 (ipykernel) as a horde alias"
+        rm -rf ${kernel_path}/python3
+        dir="${kernel_path}/python3"
+        file="${dir}/kernel.json"
+        cp -rf ${kernel_path}/../_template ${dir}
+        sed -i 's/DISPLAY_NAME/'"Python3 (ipykernel)"'/g' ${file}
+        sed -i 's/PYTHON_MAMBA_NAME/'"horde"'/g' ${file}
+        
+        dir="${kernel_path}/horde"
+        file="${dir}/kernel.json"
+        cp -rf ${kernel_path}/../_template ${dir}
+        sed -i 's/DISPLAY_NAME/'"Horde"'/g' ${file}
+        sed -i 's/PYTHON_MAMBA_NAME/'"horde"'/g' ${file}
+    fi
 }
 
 clone_horde_worker() {
